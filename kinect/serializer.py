a789d5c08a295a169c7afaff27208084bacd820e
lunatic0696 <lunatic_soul0696@hotmail.com>
2019-09-12 12:49:47 -0300

    Serializers e começo de Views
    Serializers feitos, view de exemplo feita pra ver se é assim mesmo que deve ser feito

--- kinect/serializer.py
+++ kinect/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from kinect.models import Paciente
+from kinect.models import *
 
 
 class PacienteSerializer(serializers.ModelSerializer):
@@ -9,3 +9,33 @@ class PacienteSerializer(serializers.ModelSerializer):
     class meta:
         model = Paciente
         fields =('id', 'nome', 'telefone', 'cpf', 'dt_nascimento', 'historico', 'genero')
+
+class FisioterapeutaSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    class meta:
+        model = Fisioterapeuta
+        fields =('id', 'nome', 'telefone', 'clinica', 'descricao', 'crm', 'dt_nascimento')
+
+class ExercicioSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    class meta:
+        model = Exercicio
+        fields =('id', 'nome', 'parteDoCorpo')
+
+class TratamentoSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    class meta:
+        model = Tratamento
+        fields =('id', 'fisioterapeuta', 'paciente', 'condicao', 'avaliacao')
+
+class SessaoSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    class meta:
+        model = Sessao
+        fields =('id', 'dt_realizada', 'paciente', 'tratamento', 'exercicio')
+
+class TempoSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    class meta:
+        model = Tempo
+        fields =('id', 'sessao', 'tempo')
--- kinect/views.py
+++ kinect/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
 from rest_framework.response import Response
@@ -10,9 +11,24 @@ from kinect.models import Paciente
 from kinect.serializer import PacienteSerializer
 
 
-class PacienteList(APIView):
+"""class PacienteList(APIView):
 
     def get(self, request):
         queryset = Paciente.objects.all()
         serializer_class = PacienteSerializer(queryset, many=True)
         return Response(serializer_class.data)
+"""
+
+
+@api_view(['GET', 'POST'])
+def PacienteList(request):
+    if request.method == 'GET':
+        pacientes = Paciente.objects.all()
+        pacientes_serializer = PacienteSerializer(pacientes, many=True)
+        return Response(pacientes_serializer.data)
+    elif request.method == 'POST':
+        pacientes_serializer = PacienteSerializer(data=request.data)
+        if pacientes_serializer.is_valid():
+            pacientes_serializer.save()
+            return Response(pacientes_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(pacientes_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
