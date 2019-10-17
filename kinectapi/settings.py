bc71f5de981500bc29ec0aa6d4b6503b46b0d206
jesusadadfilho <jesusadadfilho@gmail.com>
2019-10-17 16:26:25 -0300

    Config to  deploy

--- kinectapi/settings.py
+++ kinectapi/settings.py
@@ -135,4 +135,6 @@ LOGOUT_REDIRECT_URL = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
